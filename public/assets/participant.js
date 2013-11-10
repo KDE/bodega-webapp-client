b37ae186b71550d7eb6eeb0f1b0676ea3ca31edf
Giorgos Tsiapaliokas <terietor@gmail.com>
2013-11-10 18:18:13 +0200

    add the functionality for changing the password of the participant

--- public/assets/participant.js
+++ public/assets/participant.js
@@ -16,3 +16,20 @@ App.Participant = Ember.Object.extend({
     }
 });
 
+App.Participant.reopenClass({
+    updateInfo: function(firstName, lastName, email, middleName) {
+        var query = '';
+        query = firstName ? (query + 'firstName=' + firstName) + '&' : query;
+        query = lastName ? (query + 'lastName=' + lastName) + '&' : query;
+        query = email ? (query + 'email=' + email) + '&' : query;
+        query = middleName ? (query + 'middleName=' + middleName) + '&' : query;
+        $.ajax({ url: 'http://localhost:3001/json/participant/changeAccountDetails?' +query });
+    },
+
+    updatePassword: function(newPassword) {
+        if (newPassword) {
+            $.ajax({ url: 'http://localhost:3001/json/participant/changePassword?newPassword=' + newPassword });
+        }
+    }
+});
+
